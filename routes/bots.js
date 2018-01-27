8bbc1a4d85511b8cf863079067a211cb4c01cfeb
Brian Tanner <brian@tanner.io>
2018-01-27 04:50:22 -0500

    update resource names

--- routes/bots.js
+++ routes/bots.js
@@ -2,10 +2,10 @@ let router = require('express').Router();
 const utils = require('../utils');
 // Get gateway
 router.get('/gateway', (req, res) => {
-    return utils.wrapRequest(req.rest, 'bots', 'getGateway', res, req.params.inviteId);
+    return utils.wrapRequest(req.rest, 'bot', 'getGateway', res, req.params.inviteId);
 });
 // Get bot gateway
 router.delete('/gateway/bot', (req, res) => {
-    return utils.wrapRequest(req.rest, 'bots', 'getBotGateway', res, req.params.inviteId);
+    return utils.wrapRequest(req.rest, 'bot', 'getBotGateway', res, req.params.inviteId);
 });
 module.exports = router;
--- routes/invites.js
+++ routes/invites.js
@@ -2,10 +2,10 @@ let router = require('express').Router();
 const utils = require('../utils');
 // Get invite
 router.get('/invites/:inviteId', (req, res) => {
-    return utils.wrapRequest(req.rest, 'invites', 'getInvite', res, req.params.inviteId);
+    return utils.wrapRequest(req.rest, 'invite', 'getInvite', res, req.params.inviteId);
 });
 // Delete invite
 router.delete('/invites/:inviteId', (req, res) => {
-    return utils.wrapRequest(req.rest, 'invites', 'deleteInvite', res, req.params.inviteId);
+    return utils.wrapRequest(req.rest, 'invite', 'deleteInvite', res, req.params.inviteId);
 });
 module.exports = router;
