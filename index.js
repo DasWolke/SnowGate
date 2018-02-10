5efbc4917a6f6a6edf7636e6370d7916f9582a00
Wolke <DasWolke@users.noreply.github.com>
2018-02-10 12:02:53 +0100

    Add missing routes (#7)
    * add missing routes
    
    * add a middleware for getting the audit log header

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use((req, res, next) => {
   req.rest = snowtransfer
+  if (req.headers['X-Audit-Log-Reason']) {
+    if (req.method === 'GET' || req.path.includes('/bans') || req.path.includes('/prune')) {
+      req.query.reason = req.headers['X-Audit-Log-Reason']
+    } else {
+      req.body.reason = req.headers['X-Audit-Log-Reason']
+    }
+  }
   next()
 })
 app.use('/api/v7', require('./routes/bots'))
