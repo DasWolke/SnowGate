a031b513a7009caf32b310603f86f008d9076697
Papa <bradleyjplatt@yahoo.com>
2023-02-06 01:33:22 -0700

    (0.3.0) Rewrite 2 ft. no express: forms prob broke

--- index.js
+++ index.js
@@ -1,37 +1,73 @@
-const express = require('express')
-const multer = require('multer')
+const http = require('http')
 const { SnowTransfer, Constants } = require('snowtransfer')
-const { handleReason, handleMultipart } = require('./utils')
+const { requestBody } = require('./utils')
 const config = require('./config.json')
-const app = express()
 const Client = new SnowTransfer(config.token, config.options || {})
+const apiVerRegex = /^\/api\/v\d+/
+const contentTypeSplitter = /; */
+const FormData = require('form-data')
+const formParser = require('parse-multipart-data')
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(multer({ storage: multer.memoryStorage() }).any())
-app.use(handleReason)
-
-async function proxyAllRequests (req, res) {
+const server = http.createServer(async (req, res) => {
+  /** @type {URL} */
+  const url = new URL(req.url || '', `http://${req.headers.host}`)
+  let endpoint = url.pathname.replace(apiVerRegex, '')
   try {
-    const endpoint = req.path.replace('/api/v' + Constants.REST_API_VERSION, '')
-    const method = req.method
-    const dataType = req.get('content-type')?.includes('multipart') ? 'multipart' : 'json'
-    const data = dataType === 'json' ? (Object.keys(req.query)[0] ? req.query : req.body) : handleMultipart(req)
+    /** @type {Parameters<import("snowtransfer").SnowTransfer["requestHandler"]["request"]>["1"]} */
+    // @ts-ignore I know that it is valid. Shut up!!!
+    const method = req.method?.toLowerCase() ?? 'get'
+    const dataType = req.headers['content-type']?.includes('multipart') ? 'multipart' : 'json'
+    let data
+    if (dataType === 'json') {
+      if (method !== 'get') {
+        const body = await requestBody(req, 5000).then(d => d.toString()).catch(() => '')
+        if (body.length && (body[0] === '{' || body[0] === '[')) data = JSON.parse(body)
+        else data = body.length ? body : undefined
+      }
+    } else {
+      const body = await requestBody(req)
+      const form = formParser.parse(body, ((req.headers['content-type'] || '').split(contentTypeSplitter).find(i => i.startsWith('boundary=')) || '').slice('boundary='.length))
+      const includesFiles = form.find(i => i.filename?.startsWith('files'))
+      let newForm
+      if (includesFiles) {
+        const payloadJSON = form.find(i => i.name === 'payload_json')
+        const files = form.filter(i => i.name !== 'payload_json').map((i, ind) => ({ name: i.filename || `file ${ind}`, file: i.data }))
+        const newPayload = payloadJSON ? JSON.parse(payloadJSON.data.toString('latin1')) : {}
+        if (files.length) newPayload.files = files
+        newForm = Constants.standardMultipartHandler(newPayload)
+      } else {
+        newForm = new FormData()
+        let ind = 0
+        for (const input of form) {
+          if (input.name !== 'payload_json') newForm.append(input.name || `file ${ind++}`, input.data)
+        }
+      }
+      data = newForm
+    }
+    if (method === 'get' && data && url.search.length) endpoint += url.search
 
-    const result = await Client.requestHandler.request(endpoint, method, dataType, data)
-    return res.status(200).json(result)
+    /** @type {string | undefined} */
+    // @ts-ignore
+    const reason = req.headers['x-audit-log-reason']
+
+    const result = await Client.requestHandler.request(endpoint, method, dataType, !data && method === 'get' ? Object.fromEntries(url.searchParams.entries()) : data, reason ? { 'X-Audit-Log-Reason': reason } : undefined)
+    res.writeHead(result ? 200 : 204, { 'Content-Type': 'application/json' }).end(result ? JSON.stringify(result) : undefined)
   } catch (e) {
-    const status = e.response ? e.response.status : 500
-    const response = { status, error: e.toString() }
-    if (e.response) Object.assign(response, e.response.data)
+    const status = e.httpStatus ?? 500
+    const response = {}
+    if (e.message) response.message = e.message
+    if (e.code) response.code = e.code
 
-    return res.status(status).json(response)
+    res.writeHead(status, { 'Content-Type': 'application/json' }).end(JSON.stringify(response))
   }
-}
-app.all('*', proxyAllRequests)
+
+  console.log(`${res.statusCode || '000'} ${req.method?.toUpperCase() || 'UNK'} ${endpoint} --- ${req.socket.remoteAddress}`)
+  if (!req.destroyed) req.destroy()
+  if (!res.destroyed) res.destroy()
+})
 
 const port = config.port || 4096
 const host = config.host || '127.0.0.1'
 
-app.listen(port, host)
-console.log(`App started on ${host}:${port}`)
+server.once('listening', () => console.log(`Server listening on ${host}:${port}`))
+server.listen(port, host)
--- utils.js
+++ utils.js
@@ -1,22 +1,40 @@
-function handleReason (req, res, next) {
-  const reason = req.get('x-audit-log-reason')
-  if (reason) {
-    if (req.method === 'GET' || req.path.includes('/bans') || req.path.includes('/prune')) {
-      req.query.reason = reason
-    } else {
-      req.body.reason = reason
+/**
+ * @param {import("http").IncomingMessage} req
+ * @param {number} [timeout]
+ * @returns {Promise<Buffer>}
+ */
+function requestBody (req, timeout = 10000) {
+  if (!req.headers['content-length']) return Promise.resolve(Buffer.allocUnsafe(0))
+  const sizeToMeet = Number(req.headers['content-length'])
+  return new Promise((resolve, reject) => {
+    /** @type {NodeJS.Timeout | null} */
+    let timer = null
+    let totalSize = 0
+    /** @type {Array<Buffer>} */
+    const chunks = []
+    /** @param {Buffer} chunk */
+    function onData (chunk) {
+      totalSize += chunk.byteLength
+      if (totalSize > sizeToMeet) {
+        req.removeListener('data', onData)
+        req.removeListener('end', onEnd)
+        return reject(new Error('BYTE_SIZE_DOES_NOT_MATCH_LENGTH'))
+      }
+      chunks.push(chunk)
     }
-  }
-  next()
-}
-
-function handleMultipart (req) {
-  const body = JSON.parse(req.body.payload_json)
-  req.files.forEach((f, i) => {
-    body.files[i].file = f.buffer
-    body.files[i].name = f.originalname
+    function onEnd () {
+      if (timer) clearTimeout(timer)
+      req.removeListener('data', onData)
+      resolve(Buffer.concat(chunks))
+    }
+    req.on('data', onData)
+    req.once('end', onEnd)
+    timer = setTimeout(() => {
+      req.removeListener('data', onData)
+      req.removeListener('end', onEnd)
+      reject(new Error('TIMEOUT_WAITING_FOR_BODY_REACHED'))
+    }, timeout)
   })
-  return body
 }
 
-module.exports = { handleReason, handleMultipart }
+module.exports = { requestBody }
